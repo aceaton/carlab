9cf07b19d556011f3393956799bfb7aaaf8b5110
Christian <hskinker@princeton.edu>
2023-05-02 23:26:43 -0400

    Xbee Communication

--- rasp_Xbee_test.py
+++ rasp_Xbee_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from digi.xbee.devices import XBeeDevice
+from digi.xbee.packets.base import DictKeys
 import time 
 import sys
 
@@ -7,22 +8,38 @@ milli = 1e-3
 
 device = XBeeDevice("/dev/ttyS0", 9600)
 
-device.open()
+#device.open()
 
-addr = device.get_16bit_addr()
+#addr = device.get_16bit_addr()
 
-print(addr)
+#addr_int = int.from_bytes(addr, 'big')
 
-#while 1:
-    #xbee_message = device.read_data()
+#print(addr_int)
+i = 0
 
-    #remote = xbee_message.remote_device
-    #remote.read_device_info()
+try: 
+    device.open()
 
+    def packet_received_callback(packet):
+        global i 
+        i += 1
+        start = time.time()
+        if i%5 == 0:
+            packet_dict = packet.to_dict()
+            api_data = packet_dict[DictKeys.FRAME_SPEC_DATA][DictKeys.API_DATA]
+            rssi = api_data[DictKeys.RSSI]
+            address16 = api_data[DictKeys.X16BIT_ADDR]
+            end = time.time()
+            print("from: {}, RSSI: {}, Iteration: {}, Time: {}".format(address16, rssi, i, end-start))
+        if i == 100:
+            i = 0
+        #print(time.time() - start)
 
+    device.add_packet_received_callback(packet_received_callback)
 
-    #data = xbee_message.data
-    #is_broadcast = xbee_message.is_broadcast
-    #timestamp = xbee_message.timestamp
+    while(1):
+        time.sleep(milli)
 
-    #time.sleep(milli)
+finally:
+    if device is not None and device.is_open():
+        device.close()
